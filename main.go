308e5cbe2c2502ff4c2a51d5b785c7243ecb7c8a
codesoap <codesoap@mailbox.org>
2020-09-26 23:12:01 +0200

    Refactor to reduce cyclomatic complexities

--- main.go
+++ main.go
@@ -35,7 +35,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, usage)
 		os.Exit(1)
 	}
-
 	if os.Args[1] != "line" && os.Args[1] != "point" && os.Args[1] != "polygon" {
 		fmt.Fprintf(os.Stderr, "Invalid subcommand: %s\n", os.Args[1])
 		os.Exit(1)
@@ -56,72 +55,41 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Could not parse radius: %s\n", err.Error())
 		os.Exit(1)
 	}
-
-	tags := make(map[string][]string)
-	for _, tag := range os.Args[5:] {
-		split := strings.SplitN(tag, "=", 2)
-		if len(split) != 2 {
-			fmt.Fprintf(os.Stderr, "Could not parse tag: %s\n", tag)
-			os.Exit(1)
-		}
-		tags[split[0]] = append(tags[split[0]], split[1])
+	tags, err := getTags()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not parse tags: %s\n", err.Error())
+		os.Exit(1)
 	}
 
-	query := fmt.Sprintf("SELECT * FROM planet_osm_%s", os.Args[1])
-	poly := getBoundaryPolygon(lat, long, radius)
-	query += fmt.Sprintf("\nWHERE way && ST_Transform(ST_GeomFromText('%s', 4326), 3857)", poly)
-	query += getTagsFilter(tags)
-
-	rows, err := pool.Query(query)
+	rows, err := queryDB(lat, long, radius, tags)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to query database: %s\n", err.Error())
 		os.Exit(1)
 	}
-	columnNames, err := rows.Columns()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read column names: %s\n", err.Error())
+	if err = printResults(rows); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to print results: %s\n", err.Error())
 		os.Exit(1)
 	}
-	columns := make([]interface{}, len(columnNames))
-	columnPointers := make([]interface{}, len(columnNames))
-	for i := range columns {
-		columnPointers[i] = &columns[i]
+}
+
+func getTags() (map[string][]string, error) {
+	tags := make(map[string][]string)
+	for _, tag := range os.Args[5:] {
+		split := strings.SplitN(tag, "=", 2)
+		if len(split) != 2 {
+			return tags, fmt.Errorf("tag without value: %s\n", tag)
+		}
+		tags[split[0]] = append(tags[split[0]], split[1])
 	}
-	firstRow := true
-	for rows.Next() {
-		if !firstRow {
-			fmt.Println("")
-		}
-		if err := rows.Scan(columnPointers...); err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to read row: %s\n", err.Error())
-			os.Exit(1)
-		}
-		for i, columnName := range columnNames {
-			if columnName == "z_order" || columnName == "way_area" || columnName == "way" {
-				// Those columns are not for displaying.
-			} else if columnName == "osm_id" {
-				val := columnPointers[i].(*interface{})
-				id := (*val).(int64)
-				fmt.Printf("%s: %d\n", columnName, id)
-				if id < 0 {
-					id = -id // Seems to be necessary for the links.
-				}
-				switch os.Args[1] {
-				case "point":
-					fmt.Printf("osm_link: https://www.openstreetmap.org/node/%d\n", id)
-				case "line":
-					fmt.Printf("osm_link: https://www.openstreetmap.org/way/%d\n", id)
-				case "polygon":
-					fmt.Printf("osm_link: https://www.openstreetmap.org/relation/%d\n", id)
-				}
-			} else {
-				val := columnPointers[i].(*interface{})
-				valString, _ := (*val).(string) // Second return value is used to accept nil.
-				fmt.Printf("%s: %s\n", columnName, valString)
-			}
-		}
-		firstRow = false
-	}
+	return tags, nil
+}
+
+func queryDB(lat, long, radius float64, tags map[string][]string) (*sql.Rows, error) {
+	query := fmt.Sprintf("SELECT * FROM planet_osm_%s\n", os.Args[1])
+	poly := getBoundaryPolygon(lat, long, radius)
+	query += fmt.Sprintf("WHERE way && ST_Transform(ST_GeomFromText('%s', 4326), 3857)", poly)
+	query += getTagsFilter(tags)
+	return pool.Query(query)
 }
 
 func getBoundaryPolygon(lat, long, radius float64) string {
@@ -141,8 +109,6 @@ func getBoundaryPolygon(lat, long, radius float64) string {
 }
 
 func getTagsFilter(tags map[string][]string) (filter string) {
-	if len(tags) > 0 {
-	}
 	for tag, values := range tags {
 		filter += "\nAND ("
 		for i, value := range values {
@@ -154,4 +120,58 @@ func getTagsFilter(tags map[string][]string) (filter string) {
 		filter += ")"
 	}
 	return
+}
+
+func printResults(rows *sql.Rows) error {
+	colNames, err := rows.Columns()
+	if err != nil {
+		return fmt.Errorf("failed to read col names: %s\n", err.Error())
+	}
+	cols := make([]interface{}, len(colNames))
+	colPtrs := make([]interface{}, len(colNames))
+	for i := range cols {
+		colPtrs[i] = &cols[i]
+	}
+	firstRow := true
+	for rows.Next() {
+		if !firstRow {
+			fmt.Println("")
+		}
+		if err = printResult(colNames, colPtrs, rows); err != nil {
+			return err
+		}
+		firstRow = false
+	}
+	return nil
+}
+
+func printResult(colNames []string, colPtrs []interface{}, rows *sql.Rows) error {
+	if err := rows.Scan(colPtrs...); err != nil {
+		return fmt.Errorf("failed to read row: %s\n", err.Error())
+	}
+	for i, colName := range colNames {
+		if colName == "z_order" || colName == "way_area" || colName == "way" {
+			// Those columns are not for displaying.
+		} else if colName == "osm_id" {
+			val := colPtrs[i].(*interface{})
+			id := (*val).(int64)
+			fmt.Printf("%s: %d\n", colName, id)
+			if id < 0 {
+				id = -id // Seems to be necessary for the links.
+			}
+			switch os.Args[1] {
+			case "point":
+				fmt.Printf("osm_link: https://www.openstreetmap.org/node/%d\n", id)
+			case "line":
+				fmt.Printf("osm_link: https://www.openstreetmap.org/way/%d\n", id)
+			case "polygon":
+				fmt.Printf("osm_link: https://www.openstreetmap.org/relation/%d\n", id)
+			}
+		} else {
+			val := colPtrs[i].(*interface{})
+			valString, _ := (*val).(string) // Second return value is used to accept nil.
+			fmt.Printf("%s: %s\n", colName, valString)
+		}
+	}
+	return nil
 }
